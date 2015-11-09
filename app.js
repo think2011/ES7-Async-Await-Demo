a68b2299ec23b737a1e9db864d99f6453fe30607
think2011 <452125301@qq.com>
2015-11-09 17:30:12 +0800

    Fixed ┏ (゜ω゜)=☞ 排除非电影文件呗

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ import fs from 'fs';
 import path from 'path';
 import request from 'request';
 
-var movieDir = __dirname + '/movies';
+var movieDir = __dirname + '/movies',
+    exts     = ['.mkv', '.avi', '.mp4', '.rm', '.rmvb', '.wmv'];
 
 // 读取文件列表
 var readFiles = function () {
     return new Promise(function (resolve, reject) {
         fs.readdir(movieDir, function (err, files) {
-            resolve(files.map((v) => path.parse(v).name));
+            resolve(files.filter((v) => exts.includes(path.parse(v).ext)));
         });
     });
 };
@@ -36,9 +37,12 @@ var savePoster = function (movieName, url) {
     let files = await readFiles();
 
     // await只能使用在原生语法
-    for (var name of files) {
+    for (var file of files) {
+        let name = path.parse(file).name;
+
+        console.log(`正在获取【${name}】的海报`);
         savePoster(name, await getPoster(name));
     }
 
-    console.log('获取海报完成');
+    console.log('=== 获取海报完成 ===');
 })();
