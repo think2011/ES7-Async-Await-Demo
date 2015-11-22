0b608d1aaf2cf94a55f680564c27552e0ecb4c4a
think2011 <452125301@qq.com>
2015-11-22 21:44:07 +0800

    Updated 删除不存在的电影海报

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ import colog from 'colog';
 import request from 'request';
 
 var movieDir = process.argv[2] || __dirname + '/movies',
-    exts     = ['.mkv', '.avi', '.mp4', '.rm', '.rmvb', '.wmv'];
+    exts     = {
+        movie: ['.mkv', '.avi', '.mp4', '.rm', '.rmvb', '.wmv'],
+        image: ['.jpg']
+    };
 
 // 读取文件列表
-var readFiles = function () {
+var readFiles = function (exts) {
     return new Promise(function (resolve, reject) {
         fs.readdir(movieDir, function (err, files) {
             resolve(files.filter(v => exts.includes(path.parse(v).ext)));
@@ -56,7 +59,16 @@ var savePoster = async function (movieName) {
 
 
 (async () => {
-    let files = await readFiles();
+    let files  = (await readFiles(exts.movie)).map(name => path.parse(name).name),
+        images = await readFiles(exts.image);
+
+    // 删除不存在的电影海报
+    for (var image of images) {
+        let imageName = path.parse(image).name,
+            isExist   = files.indexOf(imageName) !== -1;
+
+        !isExist && fs.unlink(path.join(movieDir, image), err => colog.error(`删除【${imageName}】的海报`));
+    }
 
     for (var file of files) {
         let movieName = path.parse(file).name;
